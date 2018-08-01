3e4424b6de3546b858a85ed0ce53bb8b6fe9077d
Abdul Said <abdulrahman.asaid@gmail.com>
2018-08-01 18:06:55 -0400

    fixed recipe.js

--- assets/javascript/recipe.js
+++ assets/javascript/recipe.js
@@ -31,7 +31,11 @@ $('form').on('submit', event => {
         var readyInMins = recipeInfo[i].readyInMinutes;
         var servings = recipeInfo[i].servings;
         var recipeDiv = $('#recipe');
+        var recipeimg = recipeInfo[i].image;
+        console.log(recipeimg);
         var recipeImage = $('#recipeImage');
+        recipeImage.attr('src', recipeimg);
+        console.log(recipeImage);
 
         var spoonacularRecipeURL =
           'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/' +
@@ -47,12 +51,17 @@ $('form').on('submit', event => {
           }
         })
           .then(function(recipeResponse) {
-            // debugger;
-            console.log(recipeResponse);
+            var responseofrecipe = recipeResponse.results;
+            console.log(responseofrecipe);
             var recipeImageURL = JSON.stringify(recipeResponse.image);
             var recipeURL = recipeResponse.spoonacularSourceUrl;
+            var recipeIDvar = $('#recipeId');
+            recipeIDvar.attr('href', recipeURL);
+            console.log(recipeIDvar);
+            console.log(recipeURL);
             recipeDiv.attr('href', recipeURL);
             recipeImage.attr('src', recipeResponse.image);
+            console.log(recipeImage);
             //   console.log("imgsrc", recipeResponse.image);
             console.log(recipeImage.attr('src'));
           })
@@ -62,16 +71,20 @@ $('form').on('submit', event => {
         var recipeFavButton = '';
         var displayCard =
           "<div class='col-lg-4 col-md-4 col-sm-4 card text-center resultSection'><div class='imgContainer'> <img class='card-img-top' src=" +
-          recipeImage.attr('src') +
+          recipeImage +
           "> <br> <ul class='list-group list-group-flush'> <li class='list-group-item' id='recipeTitle'><strong>Recipe Name: </strong><span id='recipeTitle'>" +
           recipeTitle +
           "</span></li> <li class='list-group-item'><strong>Ready in: </strong><span id='readyInMins'>" +
           readyInMins +
+          ' mins' +
           "</span></li> <li class='list-group-item'><strong>Servings: </strong><span id='servings'>" +
           servings +
-          "class='btn btn-sm btn-info'><i class='fa fa-code' aria-hidden='true'> Here's the recipe! </a></div><div class='card-body justify-content-center'> <center> <a target=“_blank” href='" +
+          '</span></li></ul> <div class="card-body justify-content-center"> <center> <a target="_blank" href="' +
+          //this should be the recipe button not yet made //
           recipeFavButton +
-          "class='btn btn-sm btn-info'><i class='fa fa-code' aria-hidden='true'>Favorites THIS!</a></div>";
+          '" class="btn btn-sm btn-info"><i class="fa fa-code" aria-hidden="true">Check Out The Recipe!</a></div> <div class="card-body justify-content-center"> <center> <a target="_blank" href="' +
+          recipeFavButton +
+          '" class="btn btn-sm btn-info"><i class="fa fa-code" aria-hidden="true">Favorites THIS!</a></div>';
 
         console.log(readyInMins);
         console.log(servings);
@@ -84,53 +97,3 @@ $('form').on('submit', event => {
       console.log('Error: ' + error);
     });
 });
-
-//         var card =
-//           ‘<div class=“col-lg-4 col-md-4 col-sm-4 card text-center resultSection”  ><div class=“imgContainer”> <img class=“card-img-top” src=’ +
-//           restPic +
-//           ‘><br> <ul class=“list-group list-group-flush”> <li class=“list-group-item” id=“restauName”><strong>Restaurant Name: </strong><span id=“restauName”>’ +
-//           restauName +
-//           ‘</span></li><li class=“list-group-item” id=“restLocation”><strong>Restaurant Address: </strong><span id=“location”>’ +
-//           restLocation +
-//           ‘</span></li> <li class=“list-group-item”><strong>Rating: </strong><span id=“rating”>’ +
-//           restRating +
-//           ‘</span></li> <li class=“list-group-item”><strong>Main cuisine type: </strong><span id=“cuisine”>’ +
-//           restCouisines +
-//           ‘</span></li> <li class=“list-group-item”><strong>Average Cost for Two: </strong><span id=“restAvgCost”>’ +
-//           restAvgCost +
-//           ‘</span></li> <li class=“list-group-item”><strong>Currency restaurant deals in: </strong><span id=“restCurrency”>’ +
-//           restCurrency +
-//           ‘</span></li></ul> <div class=“card-body justify-content-center”> <center> <a target=“_blank” href=“’ +
-//           restMenu +
-//           ‘” class=“btn btn-sm btn-info”><i class=“fa fa-code” aria-hidden=“true”> Checkout Their Menu here</a></div><div class=“card-body justify-content-center”> <center> <a target=“_blank” href=“’ +
-//           favoriteButton +
-//           ‘” class=“btn btn-sm btn-info”><i class=“fa fa-code” aria-hidden=“true”>Favorites THIS!</a></div>‘;
-
-//         console.log(restauName);
-//         console.log(restPic);
-//         console.log(restLocation);
-//         console.log(restCouisines);
-//         console.log(restRating);
-//         console.log(restAvgCost);
-//         console.log(restCurrency);
-//         console.log(restMenu);
-
-//         $(‘#root’).append(card);
-//       }
-//     }
-//   });
-//   $(‘#root’).empty();
-//  });
-
-//   unirest
-//     .get(
-//       "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/menuItems/search?maxCalories=5000&maxCarbs=100&maxFat=100&maxProtein=100&minCalories=0&minCarbs=0&minFat=0&minProtein=0&number=10&offset=0&query=burger"
-//     )
-//     .header(
-//       "X-Mashape-Key",
-//       "aJ0NhhB8Q8msh12jySvBgX34nXYmp1u8MVrjsna6Y4XHsKe8Bn"
-//     )
-//     .header("Accept", "application/json")
-//     .end(function(result) {
-//       console.log(result.status, result.headers, result.body);
-//     });
