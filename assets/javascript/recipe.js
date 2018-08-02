0c4ceeded68315b3f9fd24cb5a01d6a07285ed9d
Tals <natalia.mahabir@gmail.com>
2018-08-01 23:18:28 -0400

    no longer object with img and code cleaned up, pop up windows only routing to thesearch page

--- assets/javascript/recipe.js
+++ assets/javascript/recipe.js
@@ -46,17 +46,14 @@ $(document).ready(function() {
 
           var readyInMins = recipeInfo[i].readyInMinutes;
           var servings = recipeInfo[i].servings;
-          var recipeDiv = $("#recipe");
-          var recipeimg = recipeInfo[i].image;
-          console.log(recipeimg);
-          var recipeImage = $("#recipeImage");
-          recipeImage.attr("src", recipeimg);
-          console.log(recipeImage);
+          var recipeDiv = "";
+
+          var recipeImage = "";
 
           var spoonacularRecipeURL =
             "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" +
             recipeID +
-            "/information?includeNutrition=true";
+            "/information?includeNutrition=flase";
 
           $.ajax({
             url: spoonacularRecipeURL,
@@ -67,46 +64,47 @@ $(document).ready(function() {
             }
           })
             .then(function(recipeResponse) {
-              var responseofrecipe = recipeResponse.results;
-              console.log(responseofrecipe);
-              var recipeImageURL = JSON.stringify(recipeResponse.image);
+              // var responseofrecipe = recipeResponse.results;
+              console.log(recipeResponse);
+              var recipeImageURL = recipeResponse.image;
+
               var recipeURL = recipeResponse.spoonacularSourceUrl;
-              var recipeIDvar = $("#recipeId");
-              recipeIDvar.attr("href", recipeURL);
-              console.log(recipeIDvar);
+              recipeDiv = recipeURL;
               console.log(recipeURL);
-              recipeDiv.attr("href", recipeURL);
-              recipeImage.attr("src", recipeResponse.image);
-              console.log(recipeImage);
-              //   console.log("imgsrc", recipeResponse.image);
-              console.log(recipeImage.attr("src"));
+
+              recipeImage = recipeImageURL;
+              console.log(recipeImageURL);
+
+              console.log("RECIPEDIV: " + recipeDiv);
+              console.log("RECIPEIMAGE: " + recipeImage);
             })
             .catch(function(error) {
               console.log("Recipe Error: " + error);
             });
           var recipeFavButton = "";
           var displayCard =
-            "<div class='col-lg-4 col-md-4 col-sm-4 card text-center resultSection'><div class='imgContainer'> <img class='card-img-top' src=" +
+            '<div class="col-lg-4 col-md-4 col-sm-4 card text-center resultSection"  ><div class="imgContainer"> <img class="card-img-top" src="' +
             recipeImage +
-            "> <br> <ul class='list-group list-group-flush'> <li class='list-group-item' id='recipeTitle'><strong>Recipe Name: </strong><span id='recipeTitle'>" +
+            '" ><br> <ul class="list-group list-group-flush"> <li class="list-group-item" id="recipeTitle"><strong>Recipe Name: </strong><span id="restauName">' +
             recipeTitle +
-            "</span></li> <li class='list-group-item'><strong>Ready in: </strong><span id='readyInMins'>" +
+            '</span></li><li class="list-group-item" id="readyInMins"><strong>Ready in Minutes: </strong><span id="location">' +
             readyInMins +
-            " mins" +
-            "</span></li> <li class='list-group-item'><strong>Servings: </strong><span id='servings'>" +
+            '</span></li> <li class="list-group-item"><strong>Servings: </strong><span id="servings">' +
             servings +
             '</span></li></ul> <div class="card-body justify-content-center"> <center> <a target="_blank" href="' +
-            //this should be the recipe button not yet made //
+            recipeDiv +
+            '" class="btn btn-sm btn-info"> Checkout Their Menu here! </a> </div><div class="card-body justify-content-center"> <center> <a target="_blank" href="' +
+            // this should be the favourite button, not made yet
             recipeFavButton +
-            '" class="btn btn-sm btn-info"><i class="fa fa-code" aria-hidden="true">Check Out The Recipe!</a></div> <div class="card-body justify-content-center"> <center> <a target="_blank" href="' +
-            recipeFavButton +
-            '" class="btn btn-sm btn-info"><i class="fa fa-code" aria-hidden="true">Favorites THIS!</a></div>';
+            '" class="btn btn-sm btn-info"><i class="fa fa-code" aria-hidden="true"> Favourite This! </a></div>';
 
           console.log(readyInMins);
           console.log(servings);
           //   console.log(displayCard);
 
           $("#recipeRoot").append(displayCard);
+          userRecipeSearch = "";
+          $("recipeSearchInput").val("");
         }
       })
       .catch(function(error) {
@@ -116,140 +114,140 @@ $(document).ready(function() {
 
   /*the correct version of the JS should upload*/
 
-  //Sign-in function
-  $("#signIn").on("click", function() {
-    event.preventDefault();
-    //Grabs user input values for email and password
-    var email = $("#username")
-      .val()
-      .trim();
-    var password = $("#password")
-      .val()
-      .trim();
+  // //Sign-in function
+  // $("#signIn").on("click", function() {
+  //   event.preventDefault();
+  //   //Grabs user input values for email and password
+  //   var email = $("#username")
+  //     .val()
+  //     .trim();
+  //   var password = $("#password")
+  //     .val()
+  //     .trim();
 
-    //Clears fields
-    $("#username").val("");
-    $("#password").val("");
+  //   //Clears fields
+  //   $("#username").val("");
+  //   $("#password").val("");
 
-    //If email and password absent, display error message
-    if (!email || !password) {
-      $("#signInError").modal("show");
-      //Else sign user in
-    } else {
-      var auth = firebase.auth();
+  //   //If email and password absent, display error message
+  //   if (!email || !password) {
+  //     $("#signInError").modal("show");
+  //     //Else sign user in
+  //   } else {
+  //     var auth = firebase.auth();
 
-      //Firebase function for signing in users
-      auth.signInWithEmailAndPassword(email, password).catch(function(error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log("signIn error", error);
-        $(".signInError").text(error);
-        $("#signInError").modal("show");
-      });
+  //     //Firebase function for signing in users
+  //     auth.signInWithEmailAndPassword(email, password).catch(function(error) {
+  //       var errorCode = error.code;
+  //       var errorMessage = error.message;
+  //       console.log("signIn error", error);
+  //       $(".signInError").text(error);
+  //       $("#signInError").modal("show");
+  //     });
 
-      //Once user is signed in, then direct them to their profile page
-      firebase.auth().onAuthStateChanged(firebaseUser => {
-        if (firebaseUser) {
-          console.log(firebaseUser);
-          console.log("logged in");
-          $(window).attr("location", "profilepage.html");
-        } else {
-          console.log("not logged in");
-        }
-      });
-    }
-  });
-
-  // database.ref().once("child_added", function(childSnapshot) {
-  //   var accountUser = childSnapshot.val().userName;
-  //   $("#accountName").append("<p>" + accountUser + "</p>");
-  //   console.log(accountUser);
+  //     //Once user is signed in, then direct them to their profile page
+  //     firebase.auth().onAuthStateChanged(firebaseUser => {
+  //       if (firebaseUser) {
+  //         console.log(firebaseUser);
+  //         console.log("logged in");
+  //         $(window).attr("location", "profilepage.html");
+  //       } else {
+  //         console.log("not logged in");
+  //       }
+  //     });
+  //   }
   // });
 
-  //Create account function
-  $("#createAccount").on("click", function() {
-    //validate email entry
-    event.preventDefault();
+  // // database.ref().once("child_added", function(childSnapshot) {
+  // //   var accountUser = childSnapshot.val().userName;
+  // //   $("#accountName").append("<p>" + accountUser + "</p>");
+  // //   console.log(accountUser);
+  // // });
 
-    //Grabs values from username, email, and password fields
-    var username = $("#inputUsername")
-      .val()
-      .trim();
+  // //Create account function
+  // $("#createAccount").on("click", function() {
+  //   //validate email entry
+  //   event.preventDefault();
 
-    var email = $("#inputEmail")
-      .val()
-      .trim();
-    var password1 = $("#inputPassword1")
-      .val()
-      .trim();
+  //   //Grabs values from username, email, and password fields
+  //   var username = $("#inputUsername")
+  //     .val()
+  //     .trim();
 
-    var password2 = $("#InputPassword2")
-      .val()
-      .trim();
+  //   var email = $("#inputEmail")
+  //     .val()
+  //     .trim();
+  //   var password1 = $("#inputPassword1")
+  //     .val()
+  //     .trim();
 
-    //Clears fields
-    $("#inputUsername").val("");
-    $("#inputEmail").val("");
-    $("#inputPassword1").val("");
-    $("#InputPassword2").val("");
+  //   var password2 = $("#InputPassword2")
+  //     .val()
+  //     .trim();
 
-    //If email and password absent, displays error message
-    if (!email || !password1) {
-      $("#accountCreateError").modal("show");
-      //If password1 and password2 (confirmation), don't match, then displays error message
-    } else if (password1 != password2) {
-      $(".accountCreateError").text("Passwords do not match. Please re-type.");
-      $("#accountCreateError").modal("show");
-      //Creates account and stores in Firebase authentication
-    } else {
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(email, password1)
-        .catch(function(error) {
-          $(".accountCreateError").text(error);
-          $("#accountCreateError").modal("show");
-          if (error.code === "auth/email-already-in-use") {
-            var credential = firebase.auth.EmailAuthProvider.credential(
-              email,
-              password1
-            );
-          }
-        });
+  //   //Clears fields
+  //   $("#inputUsername").val("");
+  //   $("#inputEmail").val("");
+  //   $("#inputPassword1").val("");
+  //   $("#InputPassword2").val("");
 
-      //Stores user information in Firebase database
-      var uid = firebase.auth().currentUser.uid;
-      firebase
-        .database()
-        .ref()
-        .child("accounts")
-        .child(uid)
-        .set({
-          email: email,
-          username: username
-        });
-    }
+  //   //If email and password absent, displays error message
+  //   if (!email || !password1) {
+  //     $("#accountCreateError").modal("show");
+  //     //If password1 and password2 (confirmation), don't match, then displays error message
+  //   } else if (password1 != password2) {
+  //     $(".accountCreateError").text("Passwords do not match. Please re-type.");
+  //     $("#accountCreateError").modal("show");
+  //     //Creates account and stores in Firebase authentication
+  //   } else {
+  //     firebase
+  //       .auth()
+  //       .createUserWithEmailAndPassword(email, password1)
+  //       .catch(function(error) {
+  //         $(".accountCreateError").text(error);
+  //         $("#accountCreateError").modal("show");
+  //         if (error.code === "auth/email-already-in-use") {
+  //           var credential = firebase.auth.EmailAuthProvider.credential(
+  //             email,
+  //             password1
+  //           );
+  //         }
+  //       });
 
-    //Detects if user signed in, then directs to their profile page
-    firebase.auth().onAuthStateChanged(firebaseUser => {
-      if (firebaseUser) {
-        console.log(firebaseUser);
-        console.log("logged in");
-        $(window).attr("location", "profilepage.html");
-      } else {
-        console.log("not logged in");
-      }
-    });
-  });
+  //     //Stores user information in Firebase database
+  //     var uid = firebase.auth().currentUser.uid;
+  //     firebase
+  //       .database()
+  //       .ref()
+  //       .child("accounts")
+  //       .child(uid)
+  //       .set({
+  //         email: email,
+  //         username: username
+  //       });
+  //   }
 
-  var unsubscribe = firebase.auth().onAuthStateChanged(firebaseUser => {
-    if (firebaseUser) {
-      console.log(firebaseUser);
-      console.log("logged in");
-    } else {
-      console.log("not logged in");
-      $(window).attr("location", "login.html");
-    }
-  });
+  //   //Detects if user signed in, then directs to their profile page
+  //   firebase.auth().onAuthStateChanged(firebaseUser => {
+  //     if (firebaseUser) {
+  //       console.log(firebaseUser);
+  //       console.log("logged in");
+  //       $(window).attr("location", "profilepage.html");
+  //     } else {
+  //       console.log("not logged in");
+  //     }
+  //   });
+  // });
 
-  unsubscribe();
+  // var unsubscribe = firebase.auth().onAuthStateChanged(firebaseUser => {
+  //   if (firebaseUser) {
+  //     console.log(firebaseUser);
+  //     console.log("logged in");
+  //   } else {
+  //     console.log("not logged in");
+  //     $(window).attr("location", "login.html");
+  //   }
+  // });
+
+  // unsubscribe();
 });
